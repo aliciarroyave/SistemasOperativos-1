35dc132936380d3ef0feedea9ffa28b603d53921
vosnemo <nemocastillo@hotmail.com>
2020-03-15 21:21:17 -0600

    ListaEnlazada13-03-20
    Interfaz y lista enlazada

---
+++ src/proyecto1/Nodo.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto1;
+
+
+import java.util.Date;
+
+/**
+ *
+ * @author GuillePC
+ */
+
+public class Nodo 
+{
+     public Nodo siguiente;
+     int Prioridad;
+     String ID;
+     int Tiempo;
+     int TiempoRe;
+     String Hora;
+       
+
+     
+      public void Nodo()
+    {
+        Prioridad=0;
+        ID = "";
+        Tiempo=0;
+        TiempoRe=0;
+       // Hora = new SimpleDateFormat("HH:mm:ss");
+        
+        siguiente= null;
+    }
+     
+    public int getPrioridad() {
+        return Prioridad;
+    }
+
+    public void setPrioridad(int Prioridad) {
+        this.Prioridad = Prioridad;
+    }
+    
+
+    public String getHora() {
+        return Hora;
+    }
+
+    public void setHora(String Hora) {
+        this.Hora = Hora;
+    }
+
+    public int getTiempo() {
+        return Tiempo;
+    }
+
+    public void setTiempo(int Tiempo) {
+        this.Tiempo = Tiempo;
+    }
+
+    public int getTiempoRe() {
+        return TiempoRe;
+    }
+
+    public void setTiempoRe(int TiempoRe) {
+        this.TiempoRe = TiempoRe;
+    }
+     
+     
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+    
+    
+    public Nodo(int Prioridad)
+            {
+             this.Prioridad = Prioridad;
+             siguiente = null;
+             
+            }
+    
+       
+   
+    
+    void setsiguiente(Nodo Siguiente)
+    {
+      siguiente= Siguiente;
+    }
+    
+    Nodo getsiguiente()
+    {
+        return siguiente;
+    }
+    
+    
+}
